fa829abc9198e862019aa5542fae46d143f378e3
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-08-21 15:11:47 -0500

    working on 1b, stopped to work

--- js/shopCtrl.js
+++ js/shopCtrl.js
@@ -1,3 +1,11 @@
 angular.module('assessment').controller('shopCtrl', function($scope,shopService,$state){
-  
+
+  shopService.getAllProducts()
+    .then(response => {
+      $scope.products = response
+      return response
+    })
+
+
+
 })
--- js/shopService.js
+++ js/shopService.js
@@ -1,3 +1,10 @@
 angular.module('assessment').service('shopService', function($http){
+
+  this.getAllProducts = function(){
+    return $http.get('https://practiceapi.devmountain.com/products')
+    .then(function(response) {
+      return response
+    })
+  }
   
 })
