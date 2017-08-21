0cfa3764ecd7065465fe24a4c8dc914ff028ef97
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-08-21 14:44:16 -0500

    initial setup and step 1a - skelaton

---
+++ js/app.js
@@ -0,0 +1,29 @@
+angular.module('assessment',['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider
+    .otherwise('/')
+
+  $stateProvider
+
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/home.html'
+    })
+
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.html'
+    })
+
+    .state('blog', {
+      url: '/blog',
+      templateUrl: 'views/blog.html'
+    })
+
+    .state('shop', {
+      url: '/shop',
+      templateUrl: 'views/shop.html'
+    })
+})
